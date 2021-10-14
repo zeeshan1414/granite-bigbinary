1ebafdcc6bd6bc10fe85d2410480a94af30bd8d6
Zeeshan Hussain <zeeshan1414@gmail.com>
2021-10-14 22:47:12 +0530

    Added optimized slug generation method

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 module Constants
+  is_sqlite_db = ActiveRecord::Base.connection_db_config.configuration_hash[:adapter] == "sqlite3"
+  DB_REGEX_OPERATOR = is_sqlite_db ? "REGEXP" : "~*"
   MAX_TASK_TITLE_LENGTH = 125
 end

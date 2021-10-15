172f77b803b25bde700f8ba089fb80a035b712b5
Zeeshan Hussain <zeeshan1414@gmail.com>
2021-10-15 20:50:52 +0530

    Added ability to delete a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  before_action :load_task, only: [:show, :update]
+  before_action :load_task, only: %i[show update destroy]
 
   def index
     tasks = Task.all
@@ -25,6 +25,15 @@ class TasksController < ApplicationController
   def update
     if @task.update(task_params)
       render status: :ok, json: { notice: "Successfully updated task." }
+    else
+      render status: :unprocessable_entity,
+        json: { error: @task.errors.full_messages.to_sentence }
+    end
+  end
+
+  def destroy
+    if @task.destroy
+      render status: :ok, json: { notice: "Successfully deleted task." }
     else
       render status: :unprocessable_entity,
         json: { error: @task.errors.full_messages.to_sentence }
--- app/javascript/src/components/Tasks/Table/TableRow.jsx
+++ app/javascript/src/components/Tasks/Table/TableRow.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-const TableRow = ({ data, showTask }) => {
+const TableRow = ({ data, destroyTask, showTask }) => {
   return (
     <tbody className="bg-white divide-y divide-gray-200">
       {data.map(rowData => (
@@ -21,6 +21,18 @@ const TableRow = ({ data, showTask }) => {
               Show
             </a>
           </td>
+          <td
+            className="px-6 py-4 text-sm font-medium
+            leading-5 text-right cursor-pointer"
+          >
+            <a
+              className="text-red-500
+              hover:text-red-700"
+              onClick={() => destroyTask(rowData.slug)}
+            >
+              Delete
+            </a>
+          </td>
         </tr>
       ))}
     </tbody>
@@ -29,6 +41,7 @@ const TableRow = ({ data, showTask }) => {
 
 TableRow.propTypes = {
   data: PropTypes.array.isRequired,
+  destroyTask: PropTypes.func,
   showTask: PropTypes.func
 };
 

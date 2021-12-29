75f9e240defd36e6e9736f14c1709aa564ad384c
Zeeshan Hussain <zeeshan1414@gmail.com>
2021-12-29 17:02:00 +0530

    Fixed order of items in ActiveRecord models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
   has_many :created_tasks, foreign_key: :task_owner_id, class_name: "Task"
   has_many :assigned_tasks, foreign_key: :assigned_user_id, class_name: "Task"
   has_many :comments, dependent: :destroy
-  has_secure_password
-  has_secure_token :authentication_token
 
   validates :name, presence: true, length: { maximum: Constants::MAX_NAME_LENGTH }
   validates :email, presence: true,
@@ -17,6 +15,9 @@ class User < ApplicationRecord
 
   before_save :to_lowercase
   before_destroy :assign_tasks_to_task_owners
+
+  has_secure_password
+  has_secure_token :authentication_token
 
   private
 
